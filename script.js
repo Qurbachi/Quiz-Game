2fc88b74dcfc073056751d24d45bbcd6876a2bb9
Damian Tworzydło <damian.tworzydlo17@gmail.com>
2020-03-03 18:23:03 +0100

    Creates iify

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
+(function() {
+  function Question(question, answers, correct) {
+      this.question = question;
+      this.answers = answers;
+      this.correct = correct;
+  }
 
-Question.prototype.displayQuestion = function() {
-    console.log(this.question);
+  Question.prototype.displayQuestion = function() {
+      console.log(this.question);
 
-    for (var i = 0; i < this.answers.length; i++) {
-        console.log(i + ': ' + this.answers[i]);
+      for (var i = 0; i < this.answers.length; i++) {
+          console.log(i + ': ' + this.answers[i]);
+      }
+  };
+
+  Question.prototype.checkAnswer = function(ans) {
+    if (ans === this.correct) {
+      console.log('This is the correct answer!');
+    } else {
+      console.log('Your answer is wrong.');
     }
-};
+  };
 
 
 
-var q1 = new Question('What is the former capital city of Poland?', ['Cracow', 'Warsaw', 'Poznań', 'Gdańsk'], 0);
-var q2 = new Question('Which club as first won the championship of Poland?', ['Wisła Kraków', 'Czarni Lwów', 'Polonia Warszawa', 'Cracovia'], 3);
-var q3 = new Question('How many regions of Poland bears live in?', ['none', 1, 2, 3], 3);
-var q4 = new Question('When Poland regained its independance?', [1917, 1918, 1919, 1920], 1);
+  var q1 = new Question('What is the former capital city of Poland?', ['Cracow', 'Warsaw', 'Poznań', 'Gdańsk'], 0);
+  var q2 = new Question('Which club as first won the championship of Poland?', ['Wisła Kraków', 'Czarni Lwów', 'Polonia Warszawa', 'Cracovia'], 3);
+  var q3 = new Question('How many regions of Poland bears live in?', ['none', 1, 2, 3], 3);
+  var q4 = new Question('When Poland regained its independance?', [1917, 1918, 1919, 1920], 1);
 
-var questions = [q1, q2, q3, q4];
+  var questions = [q1, q2, q3, q4];
 
-var n = Math.floor(Math.random() * questions.length);
+  var n = Math.floor(Math.random() * questions.length);
 
-questions[n].displayQuestion();
+  questions[n].displayQuestion();
+
+  var answer = parseInt(prompt('What is the answer?'));
+
+  questions[n].checkAnswer(answer);
+
+})();
